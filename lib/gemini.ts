8b7511724b4f4501f0b22a632ad7272d79409d33
jhones <sanchezjhones45@gmail.com>
2025-09-25 20:33:44 +0800

    fixed 2k text param limit

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -1,19 +1,27 @@
-import { GoogleGenAI } from "@google/genai"; 
+import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({ apiKey: "AIzaSyAuXGFYFLkw-QffZ4SDtPM2hnFmQISjQ3k" });
 
 export const askGemini = async (message: string) => {
-    const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
-        contents: message,
-        config: {
-            thinkingConfig: {
-                thinkingBudget: 0, // Disables thinking
-            },
-        }
-    });
+    try {
 
-    const result = response.text
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash-lite",
+            contents: `
+             Be concise.    
+            ${message}
+            `,
+            config: {
+                thinkingConfig: {
+                    thinkingBudget: 0, // Disables thinking
+                },
+            }
+        });
 
-    return result
+        const result = response.text
+
+        return result
+    } catch (error) {
+        return "An Error occured"
+    }
 }
